0239d9bb31f4ec6b11829ee378883605d9b03a68
JoseMeurer <josewm18@gmail.com>
2022-09-27 17:27:47 -0300

    initial commit

---
+++ src/main/java/com/josemeurer/devcourses/DevcoursesApplication.java
@@ -0,0 +1,13 @@
+package com.josemeurer.devcourses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevcoursesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevcoursesApplication.class, args);
+	}
+
+}
